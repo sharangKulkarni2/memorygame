75c1c075516d7b5411992f8c4a39e8107120bf4a
sharang kulkarni <45118333+sharangKulkarni2@users.noreply.github.com>
2020-09-19 22:01:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'hotdog.png'
+        },
+        {
+            name: 'fries',
+            img: 'fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'hotdog.png'
+        }
+    ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    const cardsWon = []
+
+    //create your board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'blank.png')
+            card.setAttribute('width', '200px')
+            card.setAttribute('height', '200px')
+            card.setAttribute('border', '1px solid black')
+            card.addEventListener("mouseenter", function (event) {
+                // highlight the mouseenter target
+               this.style.transform = "scale(1.15)";
+                var self = this
+                // reset the color after a short delay
+                setTimeout(function () {
+                    self.style.transform = "scale(1)";
+                }, 200);
+            }, false);
+
+
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if (optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'blank.png')
+            cards[optionTwoId].setAttribute('src', 'blank.png')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'white.png')
+            cards[optionTwoId].setAttribute('src', 'white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'blank.png')
+            cards[optionTwoId].setAttribute('src', 'blank.png')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
+
+    //flip your card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    createBoard()
+})
